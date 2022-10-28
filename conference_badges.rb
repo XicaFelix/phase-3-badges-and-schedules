d517ac6becaceb45e5c34275e6b13dba1033e92a
Xica Felix <xicasfelix@gmail.com>
2022-10-28 13:45:55 -0400

    commit tree f9c8c272d942a58fe912c2c9edffbe6ae993dfb8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+     array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms speakers
+   speakers.map.with_index(1) do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index}!"
+   end
+end
+
+def printer array
+   batch_badge_creator(array).each {|x| puts x} 
+    assign_rooms(array).each {|y| puts y}
+end
